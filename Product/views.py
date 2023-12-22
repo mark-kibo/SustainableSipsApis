156cda3e7418dca51039f42ba88f18671c3b9e48
mark-kibo <kibochamark@gmail.com>
2023-12-22 18:53:07 +0300

    Feat:get product api development

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
+
+
+class ProductViewSet(ViewSet):
+
+    def get_products(self, request):
+        queryset=Product.objects.all()
+
+        # serializer
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
