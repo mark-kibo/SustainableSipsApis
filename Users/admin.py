948f9aad65c6dd98cdf6c591d2f6a545371b1711
mark-kibo <kibochamark@gmail.com>
2023-12-28 12:29:49 +0300

    Feat:Update on user model.

--- Users/admin.py
+++ Users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Roles
+from django.contrib.auth.admin import UserAdmin
+from .models import SustainableSipsUser, Roles
 # Register your models here.
 
 
-admin.site.register(User)
+admin.site.register(SustainableSipsUser)
 admin.site.register(Roles)
--- Users/models.py
+++ Users/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+# from django.contrib.auth.models import BaseUserManager
+
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, password=None, **extra_fields):
+#         # your implementation
+
+#     def create_superuser(self, email, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+
+#         return self.create_user(email, password, **extra_fields)
 
 
 
@@ -22,21 +35,75 @@ from django.contrib.auth.models import AbstractBaseUser
 #     def check_password_hash_match(self, plain_password):
 #         return check_password(plain_password, self.password)
 
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("You have not provided a valid e-mail address")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
 class Roles(models.Model):
     role_name = models.CharField(max_length=255)
 
 
 
-class User(AbstractBaseUser, models.Model):
+
+
+class SustainableSipsUser(AbstractBaseUser, PermissionsMixin):
     user_id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
         editable=False
     )
-    username= models.CharField(max_length=255)
+    username= models.CharField(max_length=255, unique=True, null=True, blank=True, default='')
+    email= models.EmailField(max_length=255, unique=True, blank=False)
+    password= models.CharField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=20)
     role = models.ForeignKey('Roles', on_delete=models.SET_NULL, null=True, blank=True)
 
+    date_joined=models.DateTimeField(default=timezone.now)
+    date_joined=models.DateTimeField(blank=True, null=True)
+
+    is_staff=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    is_anonymous=models.BooleanField(default=False)
+    is_authenticated=models.BooleanField(default=True)
+    
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    objects=UserManager()
+
+    USERNAME_FIELD='username'
+    EMAIL_FIELD='email'
+    # PASSWORD_FIELD='password'
+    REQUIRED_FIELDS=[]
+
+
+  
 
     def set_password_hash(self, plain_password):
         self.password = make_password(plain_password)
